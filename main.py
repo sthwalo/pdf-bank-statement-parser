e954bcf3660d62806117f64b5317ea84ef0e9bfa
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-10-29 00:06:12 +0200

    feat(main.py): found opening balance

--- main.py
+++ main.py
@@ -18,19 +18,29 @@ IDENTIFY_TRANSACTION_ROW_REGEX: Final[str] = (
 )
 
 with pdfplumber.open("bank_statements/FNB_ASPIRE_CURRENT_ACCOUNT_100.pdf") as pdf:
+    opening_balance_matches: list[Decimal] = []
     for page in pdf.pages:
         page_text: str = page.extract_text()
-        found_opening_balance = re.search(
-            r"Opening Balance\s+([\d,]+\.\d{2}(?:Cr)?)\b", page_text
-        )
-        if found_opening_balance:
-            print("hello")
-            break
-            opening_balance: Decimal = Decimal(found_opening_balance.groups()[0])
-            print("found opening balance:", opening_balance)
-        # break
         for row in page_text.split("\n"):
             print(row)
+
+            found_opening_balance = re.search(
+                r"Opening Balance\s+([\d,]+\.\d{2}(?:Cr)?)\b", row
+            )
+            if found_opening_balance:
+                opening_balance_raw: str = found_opening_balance.groups()[0].replace(
+                    " ", ""
+                )
+                if "Cr" not in opening_balance_raw:
+                    opening_balance: Decimal = Decimal(
+                        "-" + opening_balance_raw.replace("Cr", "")
+                    )
+                else:
+                    opening_balance: Decimal = Decimal(
+                        opening_balance_raw.replace("Cr", "").replace(",", "")
+                    )
+                opening_balance_matches.append(opening_balance)
+
             row_match = re.match(IDENTIFY_TRANSACTION_ROW_REGEX, row.strip())
             if row_match:
                 raw_date, raw_desc, raw_amt, raw_balance, raw_fee = row_match.groups()
